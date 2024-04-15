8ff2de50619b8f3009ac2ef8e2397ab05eed0401
danvincent1122 <danvincent1122@gmail.com>
2024-04-15 19:05:52 +0800

    fix: add login and reset-password page

---
+++ platform/app/src/routes/ResetPassword/ResetPassword.tsx
@@ -0,0 +1,58 @@
+import React from 'react';
+import { Button, Input, Typography } from '@ohif/ui';
+import { useTranslation } from 'react-i18next';
+
+const ResetPasswordPage = () => {
+  const { t } = useTranslation();
+
+  return (
+    <div className="relative mx-0 grid h-screen w-screen grid-cols-12 ">
+      <div className="col-span-12  bg-[#151815] p-10 sm:col-span-8 md:col-span-6 xl:col-span-5">
+        <div className="flex h-full flex-col justify-between">
+          <div></div>
+          <div>
+            <Typography
+              variant="h3"
+              className="mt-2 text-white"
+            >
+              {t('Reset Password')}
+            </Typography>
+            <Typography
+              variant="body"
+              className="mb-5 text-white text-opacity-70"
+            >
+              {t('Provide your new password for abc@gmail.com account.')}
+            </Typography>
+            <Input
+              placeholder="New Password"
+              autoFocus
+              id="newPassword"
+              className="mb-4 w-full"
+              type="password"
+            />
+            <Input
+              placeholder="Confirm New Password"
+              autoFocus
+              id="confirmNewPassword"
+              className="mb-4 w-full"
+              type="password"
+            />
+            <Button
+              disabled={false}
+              className="mt-7 h-[51px] w-full rounded-lg"
+            >
+              {'Confirm'}
+            </Button>
+          </div>
+          <div>
+            <h1 className="text-center text-base text-white text-opacity-70">
+              © 2024 PACS AI. All rights reserved.
+            </h1>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ResetPasswordPage;
