831bb808ff4de030f2110a23ac353ec5e2cb04aa
danvincent1122 <danvincent1122@gmail.com>
2024-04-18 15:44:07 +0800

    fix: update select dropdown design

--- platform/ui/src/components/WindowLevelMenuItem/WindowLevelMenuItem.tsx
+++ platform/ui/src/components/WindowLevelMenuItem/WindowLevelMenuItem.tsx
@@ -6,13 +6,13 @@ const WindowLevelMenuItem = ({ title, subtitle, isSelected, index }) => (
   <>
     <div
       className={classNames(
-        'hover:bg-primary-dark flex h-8 w-full flex-row items-center p-3',
-        isSelected && 'bg-primary-dark'
+        'hover:bg-primary-dark flex h-8 w-full flex-row items-center p-3 text-white hover:!text-black',
+        isSelected && 'bg-primary-dark !text-black'
       )}
     >
-      <span className="text-common-bright mr-2 whitespace-nowrap text-base">{title}</span>
-      <span className="text-aqua-pale flex-1 whitespace-nowrap text-sm font-light">{subtitle}</span>
-      <span className="text-primary-active ml-5 whitespace-nowrap text-sm">{index + 1}</span>
+      <span className="mr-2 whitespace-nowrap text-base">{title}</span>
+      <span className="flex-1 whitespace-nowrap text-sm font-light">{subtitle}</span>
+      <span className="ml-5 whitespace-nowrap text-sm">{index + 1}</span>
     </div>
   </>
 );
