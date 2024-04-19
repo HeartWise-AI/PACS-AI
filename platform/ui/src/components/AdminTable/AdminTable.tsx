d69cc13f4df64f92e264ef870e4a322b7da1a317
danvincent1122 <danvincent1122@gmail.com>
2024-04-19 15:35:32 +0800

    fix: add admin table component

---
+++ platform/ui/src/components/AdminTable/AdminTable.tsx
@@ -0,0 +1,42 @@
+import React, { useState } from 'react';
+import { Typography } from '@ohif/ui';
+import { useTranslation } from 'react-i18next';
+
+const AdminTable = ({ headers, data }) => {
+  return (
+    <table className="w-full text-left text-sm rtl:text-right ">
+      <thead className="bg-transparent text-sm font-light text-white text-opacity-50">
+        <tr>
+          {headers.map((header, index) => (
+            <th
+              key={index}
+              scope="col"
+              className="px-6 py-3"
+            >
+              {header}
+            </th>
+          ))}
+        </tr>
+      </thead>
+      <tbody>
+        {data.map((item, rowIndex) => (
+          <tr
+            key={rowIndex}
+            className={`bg-transparent text-base text-white`}
+          >
+            {item.map((cell, cellIndex) => (
+              <td
+                key={cellIndex}
+                className="px-6 py-4"
+              >
+                {cell}
+              </td>
+            ))}
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+};
+
+export default AdminTable;
