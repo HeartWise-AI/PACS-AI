cfe69bfc7e9339601d96b63bf53c7cd363c85ab9
danvincent1122 <danvincent1122@gmail.com>
2024-05-07 16:08:48 +0800

    fix(kibana-logs): ui integration

--- platform/app/src/routes/KibanaLogs/KibanaLogs.tsx
+++ platform/app/src/routes/KibanaLogs/KibanaLogs.tsx
@@ -1,59 +1,156 @@
 import React, { useState } from 'react';
 import { useTranslation } from 'react-i18next';
-import { AdminTable, Button, Input, Logo, Typography } from '@ohif/ui';
-import HeaderPanel from '/components/HeaderPanel';
+import { Button, Input, InputDateRange, InputMultiSelect, Logo, Typography } from '@ohif/ui';
+import HeaderPanel from '../../components/HeaderPanel';
 import SidebarAdmin from '../../components/SidebarAdmin';
+import Table from '../../components/Table';
+import chevronDownIcon from './../../assets/pacs/icons/chevron-down.png';
+import downloadIcon from './../../assets/pacs/icons/download-black.png';
+import { LogsAuditType } from '../../api/dto';
 
 const KibanaLogsPage = () => {
-  const headers = ['ID', 'Name', 'Email', 'License No.', 'Specialty', 'Email Status', 'Created At'];
-  const data = [
-    [
-      '556342B',
-      'Juan Dela Cruz',
-      'abc123@gmail.com',
-      'L1234567890',
-      'Sample Specialty',
-      'Verified',
-      'Oct 19, 1994 14:19',
-    ],
-    [
-      '556342B',
-      'Juan Dela Cruz',
-      'abc123@gmail.com',
-      'L1234567890',
-      'Sample Specialty',
-      'Verified',
-      'Oct 19, 1994 14:19',
-    ],
-    [
-      '556342B',
-      'Juan Dela Cruz',
-      'abc123@gmail.com',
-      'L1234567890',
-      'Sample Specialty',
-      'Verified',
-      'Oct 19, 1994 14:19',
-    ],
-    [
-      '556342B',
-      'Juan Dela Cruz',
-      'abc123@gmail.com',
-      'L1234567890',
-      'Sample Specialty',
-      'Verified',
-      'Oct 19, 1994 14:19',
-    ],
-    [
-      '556342B',
-      'Juan Dela Cruz',
-      'abc123@gmail.com',
-      'L1234567890',
-      'Sample Specialty',
-      'Verified',
-      'Oct 19, 1994 14:19',
-    ],
+  const [selectedAuditType, setSelectedAuditType] = useState<LogsAuditType>(
+    LogsAuditType.INFERENCE
+  );
+  const auditInferenceHeaders = [
+    { text: 'Author', value: 'author', align: 'left' },
+    { text: 'Timestamp', value: 'timestamp', align: 'left' },
+    { text: 'Algorithm', value: 'algorithm', align: 'center' },
+    { text: 'Python Version', value: 'pythonVersion', align: 'left' },
+    { text: 'Cuda Version', value: 'cudaVersion', align: 'left' },
+    { text: 'Pytorch Version', value: 'pytorchVersion', align: 'left' },
+    { text: 'Model Version', value: 'modelVersion', align: 'left' },
+    { text: 'MRN', value: 'mrn', align: 'left' },
+    { text: 'Study Instance UID', value: 'studyInstanceUID', align: 'left' },
+    { text: 'Accession Number', value: 'accessionNumber', align: 'center' },
+    { text: 'Series', value: 'series', align: 'left' },
+    { text: 'Series Instance UID', value: 'seriesInstanceUID', align: 'left' },
+    { text: 'Instance UID', value: 'instanceUID', align: 'left' },
+    { text: 'Detected Vessel', value: 'detectedVessel', align: 'left' },
+    { text: 'LVEF', value: 'lvef', align: 'left' },
+  ];
+  const auditDatabaseHeaders = [
+    { text: 'Author', value: 'author', align: 'left' },
+    { text: 'Timestamp', value: 'timestamp', align: 'left' },
+    { text: 'Deleted Series', value: 'deletedSeries', align: 'left' },
+  ];
+  const auditInferenceData = [
+    {
+      author: 'denis.corbin@e-mhicc.org',
+      timestamp: 'Mar 20, 2024 14:19',
+      algorithm: 'Cath-EF',
+      pythonVersion: 'Python 3.9.18',
+      cudaVersion: '12.1',
+      pytorchVersion: '2.1.0+cu121',
+      modelVersion: '1.0.0',
+      mrn: '556342B',
+      studyInstanceUID: '1.3.6.1.4.1.14538.0.101.76.6279.6001.128435439123456789',
+      accessionNumber: '-',
+      series: 'Series 1',
+      seriesInstanceUID: '1.3.6.1.4.1.14538.0.101.76.6279.6001.123456789123456789',
+      instanceUID: '1.3.6.1.4.1.14538.0.101.76.6279.6001.123456789123456789',
+      detectedVessel: 'Stenting',
+      lvef: 0,
+    },
+    {
+      author: 'denis.corbin@e-mhicc.org',
+      timestamp: 'Mar 20, 2024 14:19',
+      algorithm: 'Cath-EF',
+      pythonVersion: 'Python 3.9.18',
+      cudaVersion: '12.1',
+      pytorchVersion: '2.1.0+cu121',
+      modelVersion: '1.0.0',
+      mrn: '556342B',
+      studyInstanceUID: '1.3.6.1.4.1.14538.0.101.76.6279.6001.128435439123456789',
+      accessionNumber: '-',
+      series: 'Series 1',
+      seriesInstanceUID: '1.3.6.1.4.1.14538.0.101.76.6279.6001.123456789123456789',
+      instanceUID: '1.3.6.1.4.1.14538.0.101.76.6279.6001.123456789123456789',
+      detectedVessel: 'Stenting',
+      lvef: 0,
+    },
+    {
+      author: 'denis.corbin@e-mhicc.org',
+      timestamp: 'Mar 20, 2024 14:19',
+      algorithm: 'Cath-EF',
+      pythonVersion: 'Python 3.9.18',
+      cudaVersion: '12.1',
+      pytorchVersion: '2.1.0+cu121',
+      modelVersion: '1.0.0',
+      mrn: '556342B',
+      studyInstanceUID: '1.3.6.1.4.1.14538.0.101.76.6279.6001.128435439123456789',
+      accessionNumber: '-',
+      series: 'Series 1',
+      seriesInstanceUID: '1.3.6.1.4.1.14538.0.101.76.6279.6001.123456789123456789',
+      instanceUID: '1.3.6.1.4.1.14538.0.101.76.6279.6001.123456789123456789',
+      detectedVessel: 'Stenting',
+      lvef: 0,
+    },
   ];
 
+  const auditDatabaseData = [
+    {
+      author: 'System',
+      timestamp: 'Mar 20, 2024 14:19',
+      deletedSeries: '62cc4f41-9b1679c0-6ad19a2e-71fccb6f-07a3d16d',
+    },
+    {
+      author: 'System',
+      timestamp: 'Mar 20, 2024 14:19',
+      deletedSeries: '62cc4f41-9b1679c0-6ad19a2e-71fccb6f-07a3d16d',
+    },
+    {
+      author: 'System',
+      timestamp: 'Mar 20, 2024 14:19',
+      deletedSeries: '62cc4f41-9b1679c0-6ad19a2e-71fccb6f-07a3d16d',
+    },
+    {
+      author: 'System',
+      timestamp: 'Mar 20, 2024 14:19',
+      deletedSeries: '62cc4f41-9b1679c0-6ad19a2e-71fccb6f-07a3d16d',
+    },
+    {
+      author: 'System',
+      timestamp: 'Mar 20, 2024 14:19',
+      deletedSeries: '62cc4f41-9b1679c0-6ad19a2e-71fccb6f-07a3d16d',
+    },
+    {
+      author: 'System',
+      timestamp: 'Mar 20, 2024 14:19',
+      deletedSeries: '62cc4f41-9b1679c0-6ad19a2e-71fccb6f-07a3d16d',
+    },
+  ];
+  const AuditInferenceTable = () => {
+    return (
+      <Table
+        headers={auditInferenceHeaders}
+        data={auditInferenceData}
+      >
+        {cell => {
+          return cell;
+        }}
+      </Table>
+    );
+  };
+  const AuditDatabaseTable = () => {
+    return (
+      <Table
+        headers={auditDatabaseHeaders}
+        data={auditDatabaseData}
+      >
+        {cell => {
+          return cell;
+        }}
+      </Table>
+    );
+  };
+
+  const handleDateRangeFieldChange = ({ startDate, endDate }) => {
+    console.log({ startDate, endDate });
+  };
+  const handleFieldChange = newValue => {
+    console.log({ newValue });
+  };
   return (
     <div className="h-screen w-screen overflow-x-hidden bg-[#151815]">
       <div className="flex w-full bg-[#151815] ">
@@ -61,27 +158,116 @@ const KibanaLogsPage = () => {
         <div className="ohif-scrollbar mr-5 flex grow flex-col overflow-y-auto">
           <HeaderPanel title="Kibana Logs" />
           {/* filter container */}
-          <div className="flex justify-between rounded-xl border  border-white border-opacity-10 bg-white bg-opacity-[5%] p-5">
-            <Input
-              placeholder="Search member name, email, license no., etc."
-              autoFocus
-              id="search"
-              className="w-[40%]"
-              type="text"
-            />
-            <Button
-              disabled={false}
-              className="h-[51px] w-[51px] rounded-lg !px-0"
-            >
-              {'Q'}
-            </Button>
+          <div className="flex w-full justify-between gap-2 rounded-xl border  border-white border-opacity-10 bg-white bg-opacity-[5%] p-5">
+            <div className="-mt-2 w-1/4">
+              <InputDateRange
+                id="KibanaLogsInputDateRange"
+                label=""
+                onChange={handleDateRangeFieldChange}
+              />
+            </div>
+            {selectedAuditType === LogsAuditType.INFERENCE && (
+              <div className="flex w-[45%] items-center gap-2">
+                {/* select MRN */}
+                <div className="relative w-full">
+                  <select
+                    id="SelectMRN"
+                    className="block h-[51px] w-full cursor-pointer appearance-none rounded-lg border-2 border-none bg-white bg-opacity-10 py-3 px-3 pr-8 text-lg leading-tight text-white focus:outline-none"
+                  >
+                    {Object.values(auditInferenceData).map(item => (
+                      <option
+                        key={item.mrn}
+                        value={item.mrn}
+                        className="!cursor-pointer !bg-[#323631] !py-2"
+                      >
+                        {item.mrn}
+                      </option>
+                    ))}
+                  </select>
+                  <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
+                    <img
+                      src={chevronDownIcon}
+                      alt="Chevron down icon"
+                      className="w-5"
+                    />
+                  </div>
+                </div>
+                {/* select author */}
+                <div className="relative w-full">
+                  <select
+                    id="SelectAuthor"
+                    className="block h-[51px] w-full cursor-pointer appearance-none rounded-lg border-2 border-none bg-white bg-opacity-10 py-3 px-3 pr-8 text-lg leading-tight text-white focus:outline-none"
+                  >
+                    {Object.values(auditInferenceData).map(item => (
+                      <option
+                        key={item.author}
+                        value={item.author}
+                        className="!cursor-pointer !bg-[#323631] !py-2"
+                      >
+                        {item.author}
+                      </option>
+                    ))}
+                  </select>
+                  <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
+                    <img
+                      src={chevronDownIcon}
+                      alt="Chevron down icon"
+                      className="w-5"
+                    />
+                  </div>
+                </div>
+              </div>
+            )}
+
+            <div className="flex w-[30%] items-center gap-2">
+              {/* select audit type */}
+              <div className="relative w-full">
+                <select
+                  id="SelectAuditType"
+                  value={selectedAuditType}
+                  className="block h-[51px] w-full cursor-pointer appearance-none rounded-lg border-2 border-none bg-white bg-opacity-10 py-3 px-3 pr-8 text-lg leading-tight text-white focus:outline-none"
+                  onChange={e => setSelectedAuditType(e.target.value as LogsAuditType)}
+                >
+                  {Object.values(LogsAuditType).map(auditType => (
+                    <option
+                      key={auditType}
+                      value={auditType}
+                      className="!cursor-pointer !bg-[#323631] !py-2"
+                    >
+                      {auditType}
+                    </option>
+                  ))}
+                </select>
+                <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
+                  <img
+                    src={chevronDownIcon}
+                    alt="Chevron down icon"
+                    className="w-5"
+                  />
+                </div>
+              </div>
+              <Button
+                disabled={false}
+                className="h-[51px] min-w-[51px] rounded-lg !px-0"
+              >
+                {'Q'}
+              </Button>
+              <Button
+                disabled={false}
+                className="h-[51px] min-w-[51px] rounded-lg !px-0"
+              >
+                <img
+                  src={downloadIcon}
+                  alt="download icon"
+                  className="w-5"
+                />
+              </Button>
+            </div>
           </div>
           {/* table container */}
           <div className="mt-5 rounded-xl border border-white border-opacity-10 bg-white bg-opacity-[5%] p-5">
-            <AdminTable
-              headers={headers}
-              data={data}
-            />
+            {selectedAuditType === LogsAuditType.INFERENCE && <AuditInferenceTable />}
+            {selectedAuditType === LogsAuditType.DATABASE && <AuditDatabaseTable />}
           </div>
         </div>
       </div>
