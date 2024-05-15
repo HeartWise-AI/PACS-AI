baa3d4974f673c4aa1de4797e823a30cf61dda40
danvincent1122 <danvincent1122@gmail.com>
2024-05-15 12:50:20 +0800

    fix: import translation json file

--- platform/i18n/src/locales/fr/index.js
+++ platform/i18n/src/locales/fr/index.js
@@ -1,27 +1,43 @@
+import AboutModal from './AboutModal.json';
 import Buttons from './Buttons.json';
 import CineDialog from './CineDialog.json';
 import Common from './Common.json';
+import DataSourceConfiguration from './DataSourceConfiguration.json';
+import DatePicker from './DatePicker.json';
 import Header from './Header.json';
 import HeaderPanel from './HeaderPanel.json';
+import MeasurementTable from './MeasurementTable.json';
 import Members from './Members.json';
+import Messages from './Messages.json';
 import Onboarding from './Onboarding.json';
 import Settings from './Settings.json';
 import Sidebar from './Sidebar.json';
-import UserPreferencesModal from './UserPreferencesModal.json';
+import SidePanel from './SidePanel.json';
+import StudyBrowser from './StudyBrowser.json';
 import StudyList from './StudyList.json';
+import UserPreferencesModal from './UserPreferencesModal.json';
+import ViewportDownloadForm from './ViewportDownloadForm.json';
 
 export default {
   fr: {
+    AboutModal,
     Buttons,
     CineDialog,
     Common,
+    DataSourceConfiguration,
+    DatePicker,
     Header,
     HeaderPanel,
+    MeasurementTable,
     Members,
+    Messages,
     Onboarding,
     Settings,
     Sidebar,
+    SidePanel,
+    StudyBrowser,
     UserPreferencesModal,
     StudyList,
+    ViewportDownloadForm,
   },
 };
