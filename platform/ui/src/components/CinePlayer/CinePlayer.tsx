050371b1198a0295779d98a2ba7a9f6ef8c0d042
Adam Khamlichi <103279586+AdamKhamlichi@users.noreply.github.com>
2024-05-22 19:01:51 -0400

    Horizontal bar

--- platform/ui/src/components/CinePlayer/CinePlayer.tsx
+++ platform/ui/src/components/CinePlayer/CinePlayer.tsx
@@ -15,7 +15,6 @@ export type CinePlayerProps = {
   maxFrameRate?: number;
   stepFrameRate?: number;
   frameRate?: number;
-  onFrameRateChange: (value: number) => void;
   onPlayPauseChange: (value: boolean) => void;
   onClose: () => void;
   updateDynamicInfo?: () => void;
@@ -44,7 +43,6 @@ const CinePlayer: React.FC<CinePlayerProps> = ({
 }) => {
   const isDynamic = !!dynamicInfo?.numTimePoints;
   const [frameRate, setFrameRate] = useState(defaultFrameRate);
-  const debouncedSetFrameRate = useCallback(debounce(onFrameRateChange, 100), [onFrameRateChange]);
 
   const getPlayPauseIconName = () => (isPlaying ? 'icon-pause' : 'icon-play');
 
@@ -127,18 +125,6 @@ const CinePlayer: React.FC<CinePlayerProps> = ({
             position="top"
             className="group/fps cine-fps-range-tooltip"
             tight={true}
-            content={
-              <InputRange
-                containerClassName="h-9 px-2"
-                inputClassName="w-40"
-                value={frameRate}
-                minValue={minFrameRate}
-                maxValue={maxFrameRate}
-                step={stepFrameRate}
-                onChange={handleSetFrameRate}
-                showLabel={false}
-              />
-            }
           >
             <div className="flex items-center justify-center gap-1">
               <div className="flex-shrink-0 text-center text-sm leading-[22px] text-white">
@@ -178,7 +164,6 @@ CinePlayer.propTypes = {
   /** 'true' if playing, 'false' if paused */
   isPlaying: PropTypes.bool.isRequired,
   onPlayPauseChange: PropTypes.func,
-  onFrameRateChange: PropTypes.func,
   onClose: PropTypes.func,
   isDynamic: PropTypes.bool,
   dynamicInfo: PropTypes.shape({
