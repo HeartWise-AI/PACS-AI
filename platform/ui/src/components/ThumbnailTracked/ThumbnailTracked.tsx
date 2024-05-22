5d7aabadc98b72968c66ca27d8f075fdc3238e6a
danvincent1122 <danvincent1122@gmail.com>
2024-05-22 14:00:02 +0800

    fix: reposition tooltip

--- platform/ui/src/components/ThumbnailTracked/ThumbnailTracked.tsx
+++ platform/ui/src/components/ThumbnailTracked/ThumbnailTracked.tsx
@@ -47,13 +47,13 @@ function ThumbnailTracked({
           )}
         >
           <Tooltip
-            position="right"
+            position="bottom-left"
             content={
               <div className="flex flex-1 flex-row">
                 <div className="flex-2 flex items-center justify-center pr-4">
                   <Icon
                     name="info-link"
-                    className="text-primary-active"
+                    className="text-black"
                   />
                 </div>
                 <div className="flex flex-1 flex-col">
