e3e1f6c06bc384116adb613f786806132896f8d4
danvincent1122 <danvincent1122@gmail.com>
2024-05-24 01:12:41 +0800

    fix: integrate study list

---
+++ platform/app/src/api/orthancDTO.ts
@@ -0,0 +1,68 @@
+interface Studies {
+  accessionNumber: string;
+  modalitiesInStudy: string;
+  numberOfStudyRelatedSeries: string;
+  patientBirthDate: string;
+  patientID: string;
+  patientName: string;
+  patientSex: string;
+  queryRetrieveLevel: string;
+  referringPhysicianName: string;
+  retrieveAETitle: string;
+  specificCharacterSet: string;
+  studyDate: string;
+  studyDescription: string;
+  studyID: string;
+  studyInstanceUID: string;
+  studyTime: string;
+}
+export interface GetModalityStudiesResponse {
+  readonly queryId: string;
+  readonly studies: Studies[];
+}
+
+export interface GetModalityStudiesRequest {
+  accessionNumber: string;
+  institutionName: string;
+  modalitiesInStudy: string;
+  numberOfStudyRelatedSeries: string;
+  patientBirthDate: string;
+  patientID: string;
+  patientName: string;
+  patientSex: string;
+  referringPhysicianName: string;
+  requestingPhysician: string;
+  studyDate: string;
+  studyDescription: string;
+  studyID: string;
+  studyInstanceUID: string;
+  studyTime: string;
+}
+
+export interface GetJobInfoResponse {
+  readonly id: string;
+  readonly priority: number;
+  readonly progress: number;
+  readonly state: string;
+}
+export interface GetJobInfoRequest {
+  jobID: string;
+}
+export interface RetrieveModalityStudyRequest {
+  queryID: string;
+  answerIndex: number;
+}
+
+export interface RetrieveModalityStudyResponse {
+  readonly id: string;
+  readonly path: string;
+}
+
+export enum JobState {
+  PENDING = 'Pending',
+  RUNNING = 'Running',
+  SUCCESS = 'Success',
+  FAILURE = 'Failure',
+  PAUSED = 'Paused',
+  RETRY = 'Retry',
+}
---
+++ platform/app/src/api/orthancRepository.ts
@@ -0,0 +1,71 @@
+import type { AxiosResponse, AxiosError } from 'axios';
+import { object } from 'prop-types';
+import { APIResponse, ErrorAPIResponse } from './dto';
+import Api from '../pacsAPIAxios';
+import {
+  GetModalityStudiesRequest,
+  GetModalityStudiesResponse,
+  GetJobInfoRequest,
+  GetJobInfoResponse,
+  RetrieveModalityStudyRequest,
+  RetrieveModalityStudyResponse,
+} from './orthancDTO';
+
+const orthancRepository = {
+  /**
+   * Get modality studies
+   *
+   * @return  {GetModalityStudiesResponse}
+   */
+  async GetModalityStudies(
+    request: GetModalityStudiesRequest
+  ): Promise<APIResponse<GetModalityStudiesResponse>> {
+    return Api()
+      .post(`/v1/orthanc/modality/studies`, request)
+      .then((response: AxiosResponse<APIResponse<GetModalityStudiesResponse>>) => {
+        const { data } = response;
+        return data;
+      })
+      .catch((error: AxiosError<ErrorAPIResponse>) => {
+        const { response } = error;
+        throw response?.data !== undefined ? response.data : object;
+      });
+  },
+  /**
+   * Get job info
+   *
+   * @return  {GetJobInfoResponse}
+   */
+  async GetJobInfo(request: GetJobInfoRequest): Promise<APIResponse<GetJobInfoResponse>> {
+    return Api()
+      .get(`/v1/orthanc/job/${request.jobID}`)
+      .then((response: AxiosResponse<APIResponse<GetJobInfoResponse>>) => {
+        const { data } = response;
+        return data;
+      })
+      .catch((error: AxiosError<ErrorAPIResponse>) => {
+        const { response } = error;
+        throw response?.data !== undefined ? response.data : object;
+      });
+  },
+  /**
+   * Retrieve modality study
+   *
+   * @return  {RetrieveModalityStudyResponse}
+   */
+  async RetrieveModalityStudy(
+    request: RetrieveModalityStudyRequest
+  ): Promise<APIResponse<RetrieveModalityStudyResponse>> {
+    return Api()
+      .post(`/v1/orthanc/retrieve/query/${request.queryID}/answer/${request.answerIndex}`)
+      .then((response: AxiosResponse<APIResponse<RetrieveModalityStudyResponse>>) => {
+        const { data } = response;
+        return data;
+      })
+      .catch((error: AxiosError<ErrorAPIResponse>) => {
+        const { response } = error;
+        throw response?.data !== undefined ? response.data : object;
+      });
+  },
+};
+export default orthancRepository;
