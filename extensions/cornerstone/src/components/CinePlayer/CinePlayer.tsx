ab31df850607b013c6b37e2b38376fa9950367c5
danvincent1122 <danvincent1122@gmail.com>
2024-08-12 15:22:20 +0800

    refactor(cineplayer): undefined cineplayer

--- extensions/cornerstone/src/components/CinePlayer/CinePlayer.tsx
+++ extensions/cornerstone/src/components/CinePlayer/CinePlayer.tsx
@@ -37,11 +37,13 @@ function WrappedCinePlayer({
     }
 
     const { viewports } = viewportGridService.getState();
+    const viewport = viewports.get(viewportId);
 
     // check if viewports is undefined
     // it returns undefined when cineplayer is enabled
-    if(!viewports.get(viewportId)) {
-      return
+    if (!viewport) {
+      console.error(`No viewport found for id: ${viewportId}`);
+      return;
     }
 
     const { displaySetInstanceUIDs } = viewports.get(viewportId);
