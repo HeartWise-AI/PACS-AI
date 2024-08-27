65c59e7a2b0925e7329d989481b42ff6e832a74c
cacoool <denis.corbin@e-mhicc.org>
2024-08-26 23:12:29 -0400

    update patient IDs, only display first 10 characters

--- platform/app/src/routes/WorkList/WorkList.tsx
+++ platform/app/src/routes/WorkList/WorkList.tsx
@@ -399,9 +399,8 @@ function WorkList() {
           <button
             key={number}
             onClick={() => handlePageChange(number)}
-            className={`h-7 w-7 rounded-md ${
-              number === currentPage ? 'text-black' : 'text-white text-opacity-70'
-            }`}
+            className={`h-7 w-7 rounded-md ${number === currentPage ? 'text-black' : 'text-white text-opacity-70'
+              }`}
             style={{
               background:
                 number === currentPage
@@ -414,9 +413,8 @@ function WorkList() {
         ))}
         <button
           onClick={() => handlePageChange(currentPage + 1)}
-          className={`h-5 w-5 bg-transparent ${
-            currentPage === totalPages ? 'invisible' : 'visible'
-          }`}
+          className={`h-5 w-5 bg-transparent ${currentPage === totalPages ? 'invisible' : 'visible'
+            }`}
         >
           <img
             src={chevronRightIcon}
@@ -559,7 +557,7 @@ function WorkList() {
           <div className="mb-5 flex flex-col rounded-xl border border-white border-opacity-10 bg-white bg-opacity-[5%] p-5">
             <div className="mx-auto w-full overflow-x-auto">
               {isStudyListDataLoading &&
-              !Object.values(filterRef.current).every(value => value === '') ? (
+                !Object.values(filterRef.current).every(value => value === '') ? (
                 <div className="flex items-center justify-center p-5 text-center text-white">
                   <span className="text-lg font-normal text-opacity-70">
                     {t('Searching for data')} ...
@@ -601,15 +599,13 @@ function WorkList() {
                             onClick={() => toggleRow(index)}
                           >
                             <td
-                              className={`text-md py-2 px-4 font-normal ${
-                                expandedTableRows[index] ? 'rounded-tl-lg' : 'rounded-l-lg'
-                              }`}
+                              className={`text-md py-2 px-4 font-normal ${expandedTableRows[index] ? 'rounded-tl-lg' : 'rounded-l-lg'
+                                }`}
                             >
                               {row.patientName}
                             </td>
                             <td className="text-md py-2 px-4 font-normal">
-                              {row.patientID.substring(0, 7)}....
-                              {row.patientID.substring(row.patientID.length - 7)}
+                              {row.patientID.substring(0, 10)}
                             </td>
                             <td className="text-md py-2 px-4 font-normal">
                               {formatDate(row.studyDate)}
@@ -622,9 +618,8 @@ function WorkList() {
                             </td>
                             <td className="py-2 px-4">{row.accessionNumber}</td>
                             <td
-                              className={`py-2 px-4 text-sm font-normal ${
-                                expandedTableRows[index] ? '!rounded-tr-lg' : '!rounded-r-lg'
-                              }`}
+                              className={`py-2 px-4 text-sm font-normal ${expandedTableRows[index] ? '!rounded-tr-lg' : '!rounded-r-lg'
+                                }`}
                             >
                               {row.numberOfStudyRelatedSeries}
                             </td>
@@ -723,22 +718,22 @@ function WorkList() {
           {(jobInfo.state === JobState.PAUSED ||
             jobInfo.state === JobState.RETRY ||
             jobInfo.state === JobState.FAILURE) && (
-            <div className="mt-2">
-              <h1 className="text-white">{t('OrthancServiceProgressMessage')}</h1>
-              <div className="mt-6 flex justify-end">
-                <Button
-                  className="block h-5 w-11"
-                  onClick={() => {
-                    setIsOpenOrthancServiceModal(false);
-                    setJobInfo({ id: '', priority: 0, progress: 0, state: JobState.PENDING });
-                    setSyncingStudyProgress(0);
-                  }}
-                >
-                  {t('Okay')}
-                </Button>
+              <div className="mt-2">
+                <h1 className="text-white">{t('OrthancServiceProgressMessage')}</h1>
+                <div className="mt-6 flex justify-end">
+                  <Button
+                    className="block h-5 w-11"
+                    onClick={() => {
+                      setIsOpenOrthancServiceModal(false);
+                      setJobInfo({ id: '', priority: 0, progress: 0, state: JobState.PENDING });
+                      setSyncingStudyProgress(0);
+                    }}
+                  >
+                    {t('Okay')}
+                  </Button>
+                </div>
               </div>
-            </div>
-          )}
+            )}
         </Modal>
       </div>
     </div>
