c10fd29618078c333afacf673d4b77187b7eacba
cacoool <denis.corbin@e-mhicc.org>
2024-09-24 17:58:48 -0400

    mrn search clarification
    also remove the * after for clarification because searching for patient 123456 doesn't work on clinical pacs if you use 123* for the patient name. need to search with exact patientID for it to work

--- platform/app/src/routes/WorkList/WorkList.tsx
+++ platform/app/src/routes/WorkList/WorkList.tsx
@@ -395,7 +395,7 @@ function WorkList() {
     setStudyListFilter(prevFilter => {
       const updatedFilter = {
         ...prevFilter,
-        [field]: value ? `${value}*` : '',
+        [field]: value ? `${value}` : '',
       };
       filterRef.current = updatedFilter;
 
