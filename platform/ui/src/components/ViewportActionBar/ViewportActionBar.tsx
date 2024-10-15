368544ab4004c318c7d7d2aa2ddc92e637a76f8e
danvincent1122 <danvincent1122@gmail.com>
2024-10-15 16:35:34 +0800

    style: adjust alignment

--- platform/ui/src/components/ViewportActionBar/ViewportActionBar.tsx
+++ platform/ui/src/components/ViewportActionBar/ViewportActionBar.tsx
@@ -112,7 +112,7 @@ const ViewportActionBar = ({
       className="pointer-events-auto flex h-8 shrink-0 select-none items-center justify-between overflow-visible whitespace-nowrap px-2 text-base"
       onDoubleClick={onDoubleClick}
     >
-      <div className="flex min-w-[165px] flex-row gap-2">
+      <div className="flex min-w-[165px] flex-row gap-2 items-center">
         {getStatusComponent()}
         {!!label?.length && <span className="text-aqua-pale text-large ml-1">{label}</span>}
         <div className={separatorClasses}></div>
@@ -135,11 +135,11 @@ const ViewportActionBar = ({
           </>
         )}
       </div>
-      <div className="mx-auto flex flex-row gap-1 text-white">
+      <div className="mx-auto flex flex-row gap-1 text-white items-center">
         <span>Series: </span>
         <span>{currentSeries}</span>
       </div>
-      <div className="flex min-w-[165px] flex-row gap-2">
+      <div className="flex min-w-[165px] flex-row gap-2 items-center">
         {showArrows && (
           <>
             <Icon
