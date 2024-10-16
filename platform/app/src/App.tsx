d0539f3cf3b05f25f4ebbe95fce5ab56c4e99ff8
danvincent1122 <danvincent1122@gmail.com>
2024-10-16 15:07:39 +0800

    chore: bump version

--- platform/app/src/App.tsx
+++ platform/app/src/App.tsx
@@ -33,7 +33,7 @@ extensionManager: ExtensionManager,
 servicesManager: ServicesManager,
 hotkeysManager: HotkeysManager;
 
-const frontendVersion = 'v0.8.2-beta';
+const frontendVersion = 'v0.8.4-beta';
 const queryClient = new QueryClient();
 
 function App({ config, defaultExtensions, defaultModes }) {
