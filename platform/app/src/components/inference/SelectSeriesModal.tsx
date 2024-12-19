be0ec6c14a14ea11d4548e87880428d825a8c554
danvincent1122 <danvincent1122@gmail.com>
2024-12-19 13:22:24 +0800

    fix: use actual data in SelectSeriesModal

--- platform/app/src/components/inference/SelectSeriesModal.tsx
+++ platform/app/src/components/inference/SelectSeriesModal.tsx
@@ -13,6 +13,7 @@ interface SelectSeriesModalProps {
   data: string;
   loading: boolean;
   title: string;
+  thumbnails: any;
 }
 
 const SelectSeriesModal: React.FC<SelectSeriesModalProps> = ({
@@ -21,9 +22,12 @@ const SelectSeriesModal: React.FC<SelectSeriesModalProps> = ({
   data,
   loading,
   title,
+  thumbnails,
 }) => {
   const [mounted, setMounted] = useState(false);
   const [stepper, setStepper] = useState(1);
+  const [selectedSeries, setSelectedSeries] = useState<string[]>([]);
+  const [applyToStudy, setApplyToStudy] = useState(false);
 
   useEffect(() => {
     setMounted(true);
@@ -35,6 +39,16 @@ const SelectSeriesModal: React.FC<SelectSeriesModalProps> = ({
       onClose();
     }
   }, [onClose]);
+
+  const toggleSeriesSelection = (displaySetInstanceUID: string) => {
+    setSelectedSeries(prev => {
+      if (prev.includes(displaySetInstanceUID)) {
+        return prev.filter(id => id !== displaySetInstanceUID);
+      } else {
+        return [...prev, displaySetInstanceUID];
+      }
+    });
+  };
 
   if (!isOpen || !mounted) return null;
 
@@ -90,42 +104,56 @@ const SelectSeriesModal: React.FC<SelectSeriesModalProps> = ({
                       {/* header */}
                       <div className="align-center mt-4 flex flex-row justify-between text-[14px]">
                         <h1 className="text-white">Select Series</h1>
-                        <div className="cursor-pointer">
-                          <label
-                            htmlFor="show-annotations"
-                            className="flex items-center"
-                          >
-                            <span className="text-white">Apply to Study</span>
-                            <input
-                              id="show-annotations"
-                              data-cy="show-annotations"
-                              type="checkbox"
-                              className="ml-2"
-                            />
-                          </label>
+                        <div
+                          className="flex cursor-pointer items-center gap-2"
+                          onClick={() => setApplyToStudy(!applyToStudy)}
+                        >
+                          <h2 className="text-[14px] text-white">Apply to Study</h2>
+                          <img
+                            src={applyToStudy ? checkIcon : uncheckIcon}
+                            alt={applyToStudy ? 'check' : 'uncheck'}
+                            className="h-[18px] w-[18px]"
+                          />
                         </div>
                       </div>
 
                       {/* list of series */}
                       <div className="ml-0 mt-4 max-h-[450px] space-y-3 overflow-y-auto [&::-webkit-scrollbar]:w-1 [&::-webkit-scrollbar-thumb]:bg-[#ffffff] [&::-webkit-scrollbar-thumb]:bg-opacity-30 [&::-webkit-scrollbar-track]:bg-transparent">
-                        {[...Array(20)].map((_, i) => (
+                        {thumbnails.map(thumbnail => (
                           <div
-                            key={i}
+                            key={thumbnail.displaySetInstanceUID}
                             className="flex cursor-pointer items-center justify-between rounded-xl bg-[#7A7A7A] bg-opacity-10 p-3"
+                            onClick={() => toggleSeriesSelection(thumbnail.displaySetInstanceUID)}
                           >
                             <div className="flex items-center gap-4">
-                              <div className="h-[58px] w-[73px] rounded-lg border border-[#C8F469] bg-white bg-opacity-20"></div>
+                              <div className="h-[58px] w-[73px] rounded-lg border border-[#C8F469] bg-white bg-opacity-20">
+                                <img
+                                  src={thumbnail.imageSrc}
+                                  alt="thumbnail"
+                                  className="h-full w-full rounded-lg object-cover"
+                                />
+                              </div>
                               <div className="text-[14px]">
                                 <h1 className="inline text-[#C8F469]">Series: </h1>
-                                <h2 className="mr-2 inline text-white">6</h2>
-                                <h3 className="inline text-white">295</h3>
-                                <h4 className="block text-white opacity-70">Neck 1.0 B31s</h4>
+                                <h2 className="mr-2 inline text-white">{thumbnail.seriesNumber}</h2>
+                                <h3 className="inline text-white">{thumbnail.numInstances}</h3>
+                                <h4 className="block text-white opacity-70">
+                                  {thumbnail.description}
+                                </h4>
                               </div>
                             </div>
                             <div>
                               <img
-                                src={uncheckIcon}
-                                alt="uncheck"
+                                src={
+                                  selectedSeries.includes(thumbnail.displaySetInstanceUID)
+                                    ? checkIcon
+                                    : uncheckIcon
+                                }
+                                alt={
+                                  selectedSeries.includes(thumbnail.displaySetInstanceUID)
+                                    ? 'check'
+                                    : 'uncheck'
+                                }
                                 className="h-[18px] w-[18px]"
                               />
                             </div>
@@ -141,10 +169,17 @@ const SelectSeriesModal: React.FC<SelectSeriesModalProps> = ({
                           Cancel
                         </button>
                         <button
-                          className="rounded-lg bg-[#C8F469] px-4 py-2 text-black"
+                          className={`rounded-lg px-4 py-2 ${
+                            selectedSeries.length > 0
+                              ? 'bg-[#C8F469] text-black'
+                              : 'cursor-not-allowed bg-[#C8F469] bg-opacity-50 text-black'
+                          }`}
                           onClick={() => {
-                            setStepper(2);
+                            if (selectedSeries.length > 0) {
+                              setStepper(2);
+                            }
                           }}
+                          disabled={selectedSeries.length === 0}
                         >
                           Continue
                         </button>
@@ -153,7 +188,7 @@ const SelectSeriesModal: React.FC<SelectSeriesModalProps> = ({
                   )}
                   {stepper === 2 && (
                     <div>
-                      <h1 className="block text-white mt-4">Additional Details (optional)</h1>
+                      <h1 className="mt-4 block text-white">Additional Details (optional)</h1>
                       <div className="mt-4 flex gap-2">
                         <div className="flex h-[43px] w-[50%] items-center rounded-lg bg-[#323631] bg-opacity-10 px-4 text-[14px] text-white">
                           Siblings
@@ -227,6 +262,23 @@ SelectSeriesModal.propTypes = {
   data: PropTypes.string,
   loading: PropTypes.bool,
   title: PropTypes.string,
+  thumbnails: PropTypes.arrayOf(
+    PropTypes.shape({
+      displaySetInstanceUID: PropTypes.string.isRequired,
+      imageSrc: PropTypes.string,
+      imageAltText: PropTypes.string,
+      seriesDate: PropTypes.string,
+      seriesNumber: PropTypes.string,
+      numInstances: PropTypes.number,
+      description: PropTypes.string,
+      componentType: PropTypes.string.isRequired,
+      viewportIdentificator: PropTypes.arrayOf(PropTypes.string),
+      isTracked: PropTypes.bool,
+      dragData: PropTypes.shape({
+        type: PropTypes.string.isRequired,
+      }),
+    })
+  ),
 };
 
 export default SelectSeriesModal;
--- platform/ui/src/components/StudyBrowser/StudyBrowser.tsx
+++ platform/ui/src/components/StudyBrowser/StudyBrowser.tsx
@@ -67,6 +67,7 @@ const StudyBrowser = ({
                 positionRight={-110}
                 inferenceAvailableModels={inferenceAvailableModels}
                 loading={fetchingAvailableModels}
+                thumbnails={displaySets}
               />
               <button className="flex w-full items-center gap-2 rounded-lg bg-white bg-opacity-10 px-2 py-2">
                 <img
