12503ea62592a1ad64de21c40074aeeb27e59761
danvincent1122 <danvincent1122@gmail.com>
2025-01-03 18:52:51 +0800

    chore: bump version

--- platform/app/src/App.tsx
+++ platform/app/src/App.tsx
@@ -42,8 +42,7 @@ let commandsManager: CommandsManager,
   serviceProvidersManager: ServiceProvidersManager,
   hotkeysManager: HotkeysManager;
 
-
-const frontendVersion = 'v0.9.0-beta';
+const frontendVersion = 'v0.9.3-beta';
 const queryClient = new QueryClient();
 
 function App({
