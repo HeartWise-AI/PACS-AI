9bd309ee00c42e0d6d6428c744d1e6f8753b2cce
danvincent1122 <danvincent1122@gmail.com>
2025-01-20 18:58:48 +0800

    fix(viewer): update segmentation UI color

--- platform/ui-next/src/components/SegmentationTable/AddSegmentationRow.tsx
+++ platform/ui-next/src/components/SegmentationTable/AddSegmentationRow.tsx
@@ -27,7 +27,7 @@ export const AddSegmentationRow: React.FC<{ children?: React.ReactNode }> = ({
       onClick={() => !disabled && onSegmentationAdd('')}
     >
       {children}
-      <div className="text-primary-active group-hover:bg-secondary-dark flex items-center rounded-[4px] pl-1 group-hover:cursor-pointer">
+      <div className="text-primary-active group-hover:bg-primary/10 flex items-center rounded-[4px] pl-1 group-hover:cursor-pointer">
         <div className="grid h-[28px] w-[28px] place-items-center">
           {disabled ? <Icons.Info /> : <Icons.Add />}
         </div>
