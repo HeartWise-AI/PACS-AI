c6c5c2509b5d25715540907d0a0bc5a422a5c485
danvincent1122 <danvincent1122@gmail.com>
2025-01-21 14:13:47 +0800

    style(CinePlayer): update styling for player controls and text elements

--- platform/ui/src/components/CinePlayer/CinePlayer.tsx
+++ platform/ui/src/components/CinePlayer/CinePlayer.tsx
@@ -95,7 +95,7 @@ const CinePlayer: React.FC<CinePlayerProps> = ({
       )}
       <div
         className={
-          'border-secondary-light/60 bg-primary-dark inline-flex select-none items-center gap-2 rounded border px-2 py-2'
+          'inline-flex select-none items-center gap-2 rounded border border-white/50 bg-[#151515] px-2 py-2'
         }
       >
         <Icon
@@ -109,13 +109,13 @@ const CinePlayer: React.FC<CinePlayerProps> = ({
             {/* Add Tailwind classes for monospace font and center alignment */}
             <div className="text-[11px]">
               <span className="w-2 text-white">{dynamicInfo.timePointIndex}</span>{' '}
-              <span className="text-aqua-pale">{`/${dynamicInfo.numTimePoints}`}</span>
+              <span className="text-white/50">{`/${dynamicInfo.numTimePoints}`}</span>
             </div>
             <div className="text-aqua-pale text-xs">{dynamicInfo.label}</div>
           </div>
         )}
 
-        <div className="border-secondary-light ml-4 flex h-6 items-stretch gap-1 rounded border">
+        <div className="ml-4 flex h-6 items-stretch gap-1 rounded border border-none">
           <div
             className={`${fpsButtonClassNames} rounded-l`}
             onClick={() => handleSetFrameRate(frameRate - 1)}
