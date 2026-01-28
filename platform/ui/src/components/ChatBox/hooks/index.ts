04398aab8d5ba606e6250188b59de8b8bf3d6759
jacques <jdelfrate504@gmail.com>
2026-01-28 08:54:11 -0500

    feat(ChatBox.hooks): export useResizable hook for improved chat box resizing

--- platform/ui/src/components/ChatBox/hooks/index.ts
+++ platform/ui/src/components/ChatBox/hooks/index.ts
@@ -1,3 +1,4 @@
 export { useChatBox } from './useChatBox';
 export { useSeriesSelection } from './useSeriesSelection';
 export { useDraggable } from './useDraggable';
+export { useResizable } from './useResizable';
