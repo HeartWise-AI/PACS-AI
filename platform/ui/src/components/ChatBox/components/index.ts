b6dd005471265952bce7a83a7764c6ba2c9be2b6
jacques <jdelfrate504@gmail.com>
2026-01-28 08:19:24 -0500

    feat(ChatBox.index): create index file to export all ChatBox components for easier imports

---
+++ platform/ui/src/components/ChatBox/components/index.ts
@@ -0,0 +1,7 @@
+export { ChatHeader } from './ChatHeader';
+export { EmptyState } from './EmptyState';
+export { InputArea } from './InputArea';
+export { MessageBubble } from './MessageBubble';
+export { MessageList } from './MessageList';
+export { SeriesCarousel } from './SeriesCarousel';
+export { ThinkingIndicator } from './ThinkingIndicator';
