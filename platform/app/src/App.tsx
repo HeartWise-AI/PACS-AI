fb2a6853e78b8a3103cb6d72638917e09f6ad64e
danvincent11 <danvincent1122@gmail.com>
2026-02-24 00:22:10 +0800

    chore: bump version

--- platform/app/src/App.tsx
+++ platform/app/src/App.tsx
@@ -53,7 +53,7 @@ let commandsManager: CommandsManager,
   hotkeysManager: HotkeysManager;
 
 // NOTE: This is a PACS changes
-const frontendVersion = 'v0.55.4-beta';
+const frontendVersion = 'v0.56.0-beta';
 const queryClient = new QueryClient();
 
 // NOTE: This is a PACS changes
